cb1cc4cf795d45693c94261c831ba2fb0119c0b8
avariqfr30 <91586313+avariqfr30@users.noreply.github.com>
2021-11-10 17:51:15 +0700

    Delete Forums2 - No.3.py

--- Forums2 - No.3.py
+++
@@ -1,9 +0,0 @@
-## No. 3 ##
-f = open('/Users/avariqfrr30/pg66691.txt', 'r')
-
-w = [len(word) for line in f for word in line.rstrip().split(" ")]
-w_avg = float(sum(w))/float(len(w))
-
-print ('Average word length: ', w_avg)
-f.close()
-
